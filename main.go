8b4353c498584deb6e86e362cec1f9cd47c84f1c
nokia_lte <nokia_lte@ctdi.com>
2019-02-23 03:25:59 -0500

    graphQL is working, but signup and login are not set up yet

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/graphql-go/graphql"
@@ -10,61 +11,98 @@ import (
 
 //User starts the user object
 type User struct {
+	ID       string `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
-	//add info for sign up page
+	UserName string `json:"username"`
+}
+
+//setting up user object and into a query
+var data map[string]User
+
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"email": &graphql.Field{
+				Type: graphql.String,
+			},
+			"password": &graphql.Field{
+				Type: graphql.String,
+			},
+			"username": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+//creating a query object
+var rootQuery = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"user": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					idQuery, isOk := p.Args["id"].(string)
+					if isOk {
+						return data[idQuery], nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+
+var schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query: rootQuery,
+	},
+)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("Wrong resilt, unexpected %v", result.Errors)
+	}
+	return result
 }
 
 func main() {
-	schema, _ := graphql.NewSchema(graphql.SchemaConfig{})
-	http.HandleFunc("/signin", func(w http.ResponseWriter, r *http.Request) {
-		results := graphql.Do(graphql.Params{
-			Schema:        schema,
-			RequestString: r.URL.Query().Get("query"),
-		})
-		json.NewEncoder(w).Encode(results)
+	_ = importJSONDataFromFile("data.json", &data)
 
-		userType := graphql.NewObject(graphql.ObjectConfig{
-			Name: "User",
-			Fields: graphql.Fields{
-				"email": &graphql.Field{
-					Type: graphql.String,
-				},
-				"password": &graphql.Field{
-					Type: graphql.String,
-				},
-			},
-		})
-
-		rootQ := graphql.NewObject(graphql.ObjectConfig{
-			Name: "Query",
-			Fields: graphql.Fields{
-				"users": &graphql.Field{
-					Type: graphql.NewList(userType),
-					Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-						return nil, nil
-					},
-				},
-			},
-		})
-		fmt.Fprint(w, rootQ) //wtf is going on
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := executeQuery(r.URL.Query().Get("query"), schema)
+		json.NewEncoder(w).Encode(result)
 	})
 
+	fmt.Println("Server running on localhost:8080/graphql")
 	http.ListenAndServe(":8080", nil)
-
-	//testing because i dont fucking get GRAPHQL lets a go..mario
-
 }
 
-//Creating MockData ToDo:
-//replace this code with DB garbage.
-var user []User = []User{
-	User{
-		Email:    "test1@email.com",
-		Password: "password",
-	},
-	User{
-		Email:    "test2@email.com",
-		Password: "password1",
-	},
+func importJSONDataFromFile(fileName string, result interface{}) (isOK bool) {
+	isOK = true
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Print("Error:", err)
+		isOK = false
+	}
+	err = json.Unmarshal(content, result)
+	if err != nil {
+		isOK = false
+		fmt.Print("Error:", err)
+	}
+	return
 }
